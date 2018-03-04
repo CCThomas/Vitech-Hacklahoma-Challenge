684d7c8e0f60aa64b2e7a3bcf0642c8167202f3f
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-04 02:12:09 -0600

    Add proper Documentation to file.

--- file/file.py
+++ file/file.py
@@ -2,6 +2,11 @@ from object.patient import Patient
 
 
 def load_patients_from_csv(file_name):
+    """
+    Load Patient Data from CSV
+    :param file_name: Name of File to Load From
+    :return: List if Patient Objects
+    """
     patients = []
 
     csv_file = open("resources/" + file_name, "r")
@@ -16,7 +21,12 @@ def load_patients_from_csv(file_name):
 
 
 def save_patients_to_csv(file_name, patients, append_patients):
-    csv_file = None
+    """
+    Save Patient Objects to CSV
+    :param file_name: Name of File to Save to
+    :param patients: List of Patients Objects to write to file
+    :param append_patients: If True, Append to existing file, else create new file.
+    """
     if append_patients:
         csv_file = open("resources/" + file_name, "a")
     else:
@@ -30,28 +40,35 @@ def save_patients_to_csv(file_name, patients, append_patients):
 
 def save_patients_to_brandon(file_name, patients):
     """
-    ID as Integer
-    calculate_age
-    sex as boolean. 0 or 1.
-    Emplyement Status as Boolean. 0 or 1
-    List of pre_conditions
-    - 0 equals null
-    - 1: low
-    - 2: medium
-    - 3: high
-    People Covered as Integer
-    Annual income as Integer
-    Marrital Status as Bool
-    BMI
-    tabacco
-    gold
-    bronze
-    platinum
-    silver
-    purchased
-    - 0 through 3
+    Save Patient Objects to CSV
 
-    :return:
+    # File Format
+    00: ID as Integer
+    01: calculate_age() -> Returns age based on DOB
+    02: sex as Integer -> 0: Female, 1: Male
+    03: Employment Status as Integer -> 0: False, 1: True
+    04-14: List of pre_condition's Risk Factor
+        - 0: equals null
+        - 1: low
+        - 2: medium
+        - 3: high
+    15: People Covered as Integer
+    16: Annual Income as Integer
+    17: Marital Status as Integer -> 0: False, 1: True
+    18: calculate_bmi() -> returns BMI based on width & height
+    19: tobacco as integer -> 0: No, 1: Yes
+    20: Bronze as Integer
+    21: Silver as Integer
+    22: Gold as Integer
+    23: Platinum as Integer
+    24: Purchased as Integer
+        - 0: Bronze
+        - 1: Silver
+        - 2: Gold
+        - 3: Platinum
+
+    :param file_name: Name of File to Save to
+    :param patients: List of Patients Objects to write to file
     """
     csv_file = open("resources/" + file_name, "w")
 
