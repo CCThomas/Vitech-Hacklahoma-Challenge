ddd740b792f968af7abbc8fcccdf3c85ebf926eb
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-04 02:34:49 -0600

    Documented Object

--- object/precondition.py
+++ object/precondition.py
@@ -1,18 +1,25 @@
 class PreCondition:
 
-    def init(self):
-        """Constructor"""
+    def __init__(self):
+        """PreCondition Constructor"""
+        self.condition_name = "null"
+        self.icd_code = "null"
+        self.risk_factor = "null"
 
-    def from_csv(self, pre_condition_as_string):
+    def initialize_from_csv(self, pre_condition_as_string):
+        """
+        Initialize Pre Condition Object with Data read from CSV
+        :param pre_condition_as_string: Pre Condition Data as String
+        """
         data_split = pre_condition_as_string.split("^")
         self.condition_name = data_split[0]
         self.icd_code = data_split[1]
         self.risk_factor = data_split[2]
 
-    def from_server(self, pre_condition_as_string):
+    def initialize_from_server(self, pre_condition_as_string):
         """
-        '"condition_name":"Unspecified fracture of specified metacarpal bone with unspecified laterality","ICD_CODE":"S62.308","Risk_factor":"Low"'
-        :param pre_condition_as_string:
+        Initialize Pre Condition Object with Data read from Server
+        :param pre_condition_as_string: Pre Condition Data as String
         """
         pre_condition_split = pre_condition_as_string.split("\",\"")
         for pre_condition_data in pre_condition_split:
@@ -27,16 +34,25 @@ class PreCondition:
                 self.risk_factor = val
 
     def __str__(self):
+        """
+        :return: Return Condition Name
+        """
         return self.condition_name
 
     def get_csv_format(self):
+        """
+        :return: String of PreCondition Formatted for a CSV, similar to how it was read from the Server
+        """
         return str(self.condition_name).replace(",", "COMMA") + "^" + str(self.icd_code) + "^" + str(self.risk_factor)
 
     def get_risk_factor_as_int(self):
+        """
+        :return: Risk Factor Represented as an Integer
+        """
         if self.risk_factor == "Low":
-            return "0"
-        if self.risk_factor == "Medium":
             return "1"
-        if self.risk_factor == "High":
+        if self.risk_factor == "Medium":
             return "2"
+        if self.risk_factor == "High":
+            return "3"
 
