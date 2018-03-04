e484bde77551d986c8c6129d2a0572b80e0d17c2
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-03 20:01:17 -0600

    Init File Package

---
+++ file/file.py
@@ -0,0 +1,24 @@
+from object.patient import Patient
+
+
+def load_patients_from_csv(file_name):
+    patients = []
+
+    csv_file = open("resources/" + file_name, "r")
+    for line in csv_file.readlines():
+        patient = Patient()
+        patient.from_csv(line.split(","))
+        patients.append(patient)
+
+    csv_file.close()
+
+    return patients
+
+
+def save_patients_to_csv(file_name, patients):
+    csv_file = open("resources/" + file_name, "w")
+
+    for patient in patients:
+        csv_file.write(patient.get_csv_format())
+
+    csv_file.close()
