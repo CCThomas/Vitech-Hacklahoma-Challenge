64eb6d9e6ce66dc3e24b2af4ea0f86df622fca20
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-03 20:02:00 -0600

    update

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
 from parser.parse import parse_patient_data
+from file.file import save_patients_to_csv, load_patients_from_csv
+import time
 import sys
 
 # Verify User is Using Python 3+
@@ -7,4 +9,17 @@ if sys.version_info < (3, 0):
     print("Download Python 3: https://www.python.org/downloads/")
 
 
-parse_patient_data()
+patients = parse_patient_data()
+my_time = time.time()
+save_patients_to_csv("vitech_data.csv", patients)
+print(time.time() - my_time)
+
+
+"""patients_loaded = load_patients_from_csv("vitech_data.csv")
+if len(patients) == len(patients_loaded):
+    print(patients == patients_loaded)
+    for i in range(len(patients)):
+        print("Save", patients[i].get_csv_format())
+        print("Load", patients_loaded[i].get_csv_format())
+else:
+    print("Fail")"""
--- parser/parse.py
+++ parser/parse.py
@@ -9,35 +9,36 @@ def parse_patient_data():
     url_quote = "https://v3v10.vitechinc.com/solr/v_us_quotes/select"
     params = {"indent": "on", "q": "*:*", "wt": "json"}
 
-    # sending get request and saving the response as response object
-    my_request = requests.get(url=url_participant, params=params)
-
-    # extracting data in json format
-    data_participants = my_request.json()
-
-    # Get Number of Rows
-    # Note: Following lines of code commented out, as not to request the 1482000 patient data.
-    # rows = data["response"]["numFound"]
-    # params["rows"] = rows
-
     patients = []
-    for data_participant in data_participants["response"]["docs"]:
-        print("parsing...", data_participant)
+    times = 1
+    for id in range(1, 45000):
+        print("Parsing", times, "out of 45000")
+        times = times + 1
+        params["q"] = "id:" + str(id)
 
-        # Get Patient ID
-        patient_id = data_participant["id"]
-        params["q"] = "id:" + str(patient_id)
+        # sending get request and saving the response as response object
+        my_request = requests.get(url=url_participant, params=params)
 
-        # Get Detail data
-        my_request = requests.get(url=url_detail, params=params)
-        data_detail = my_request.json()
+        # extracting data in json format
+        data_participants = my_request.json()
 
-        # Get Quote Data
-        my_request = requests.get(url=url_quote, params=params)
-        data_quote = my_request.json()
+        for data_participant in data_participants["response"]["docs"]:
 
-        # Create Patient Object
-        patients.append(
-            Patient(data_participant, data_detail["response"]["docs"][0], data_quote["response"]["docs"][0]))
+            # Get Patient ID
+            patient_id = data_participant["id"]
+            params["q"] = "id:" + str(patient_id)
+
+            # Get Detail data
+            my_request = requests.get(url=url_detail, params=params)
+            data_detail = my_request.json()
+
+            # Get Quote Data
+            my_request = requests.get(url=url_quote, params=params)
+            data_quote = my_request.json()
+
+            # Create Patient Object
+            patient = Patient()
+            patient.from_server(data_participant, data_detail["response"]["docs"][0], data_quote["response"]["docs"][0])
+            patients.append(patient)
 
     return patients
