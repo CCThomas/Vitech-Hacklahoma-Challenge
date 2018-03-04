28fae4998dc8678cc2f1b4972deb085ecdd74ae6
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-04 04:10:22 -0600

    Removed Duplicate Block Comment

--- file/file.py
+++ file/file.py
@@ -41,32 +41,6 @@ def save_patients_to_csv(file_name, patients, append_patients):
 def save_patients_to_brandon(file_name, patients):
     """
     Save Patient Objects to CSV
-
-    # File Format
-    00: ID as Integer
-    01: calculate_age() -> Returns age based on DOB
-    02: sex as Integer -> 0: Female, 1: Male
-    03: Employment Status as Integer -> 0: False, 1: True
-    04-14: List of pre_condition's Risk Factor
-        - 0: equals null
-        - 1: low
-        - 2: medium
-        - 3: high
-    15: People Covered as Integer
-    16: Annual Income as Integer
-    17: Marital Status as Integer -> 0: False, 1: True
-    18: calculate_bmi() -> returns BMI based on width & height
-    19: tobacco as integer -> 0: No, 1: Yes
-    20: Bronze as Integer
-    21: Silver as Integer
-    22: Gold as Integer
-    23: Platinum as Integer
-    24: Purchased as Integer
-        - 0: Bronze
-        - 1: Silver
-        - 2: Gold
-        - 3: Platinum
-
     :param file_name: Name of File to Save to
     :param patients: List of Patients Objects to write to file
     """
